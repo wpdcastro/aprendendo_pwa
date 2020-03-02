9d4f9cb1ce0461e53791edbcef742665c5752028
William <wpdcastro@gmail.com>
2020-03-01 23:01:48 -0300

    sw

---
+++ sw_cached_files.js
@@ -0,0 +1,44 @@
+const cacheName = 'v1';
+
+const cacheAssets = [
+  '/index.php'
+];
+
+// Call Install Event
+self.addEventListener('install', e => {
+  console.log('Service Worker: Instalado com sucesso!');
+
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        console.log('Service Worker: Armazenando Arquivos em Cache');
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Call Activate Event
+self.addEventListener('activate', e => {
+  console.log('Service Worker: Ativado');
+  // Remove unwanted caches
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            console.log('Service Worker: Limpando o cache antigo');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Call Fetch Event
+self.addEventListener('fetch', e => {
+  console.log('Service Worker: Fetching');
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
